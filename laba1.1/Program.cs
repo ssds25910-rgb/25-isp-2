0dc85075d4e9d78b3e332e7df233f1c25d13acde
Yarssan <Yarssan@511-06>
2026-09-12 14:08:53 +0200

    Добавьте файлы проекта.

---
+++ laba1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("Введите x");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Введите a");
+double a = double.Parse(Console.ReadLine());
+double G = (Math.Pow(Math.Cos(Math.Abs(y + x)), 3) - (x + y)) / (Math.Pow(Math.Atan(x + a), 4)) * (Math.Pow (x, 4));
+Console.WriteLine($"G={G:f5}");
---
+++ laba1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Console.Write("Введите выше имя:");
+//String name = Console.ReadLine();
+//Console.WriteLine($"Меня зовут {name}");
+//Console.WriteLine("Введите x:");
+//int x= int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите y:");
+//int y= Convert.ToInt32(Console.ReadLine());
+//int s = x + y;
+//Console.WriteLine($"{x}+{y}={s}");
+
+//Console.Write("Введите угол в градусах: ");
+//double angle = double.Parse(Console.ReadLine());
+//double y = Math.Sin(angle * Math.PI / 180);
+//Console.WriteLine($"Радман y = {y:f5}");
+
+/
+
