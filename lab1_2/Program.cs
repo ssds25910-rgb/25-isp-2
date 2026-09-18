31a7dba0ddb029ead7a28d5ec5dfe68dda85f48d
sadZXcasd asdad <ssds25910@gmail.com>
2026-09-18 18:13:34 +0200

    lab1_2

---
+++ lab1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+    Console.WriteLine("Введите Длину отрезка a");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите Длину отрезка b");
+    int b = int.Parse(Console.ReadLine());
+    int d = a / b;
+    Console.WriteLine($"Количество отрезков:{d}");
+
+}
+catch (Exception ez)
+{ Console.WriteLine(ez.Message); }
--- laba1.1/Program.cs
+++ laba1.1/Program.cs
@@ -6,5 +6,5 @@ Console.Write("Введите y");
 double y = double.Parse(Console.ReadLine());
 Console.Write("Введите a");
 double a = double.Parse(Console.ReadLine());
-double G = (Math.Pow(Math.Cos(Math.Abs(y + x)), 3) - (x + y)) / (Math.Pow(Math.Atan(x + a), 4)) * (Math.Pow (x, 5));
+double G = (Math.Pow(Math.Cos(Math.Abs(y + x)), 3) - (x + y)) / (Math.Pow(Math.Atan(x + a), 4)) * (Math.Pow (x, ));
 Console.WriteLine($"G={G:f5}");
